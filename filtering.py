92feebedb078c190f32c155cc8d5e188cb57262e
Grant Yang <gyang276@berkeley.edu>
2024-05-18 01:02:24 -0400

    parallelization

--- filtering.py
+++ filtering.py
@@ -4,6 +4,7 @@ import yfinance as yf
 
 company_df = pd.read_csv("top_companies.csv")
 
+print(f'Number of companies: {len(company_df)}')
 
 '''
 Condition 1: "All companies that are determined to be part of the US equity market are included in the Russell US indexes."
@@ -13,9 +14,11 @@ on the FINRA ADF."
 "Eligible US exchanges: CBOE, NYSE, NYSE American, NASDAQ, ARCA."
 '''
 
-eligible_exchanges = ["NYSE", "NASDAQ", "AMEX", "ARCA", "CBOE"]
+eligible_exchanges = set(["NYSE", "NASDAQ", "AMEX", "ARCA", "CBOE"]) # for some reason the format in the data is different?
 
-company_df = company_df[company_df['exchange'].isin(eligible_exchanges)]
+#company_df = company_df[company_df['exchange'].isin(eligible_exchanges)]
+
+print(f'Updated number of companies after condition 1: {len(company_df)}')
 
 
 '''
@@ -30,6 +33,8 @@ the last day of the IPO eligibility period in order to qualify for index inclusi
 
 company_df = company_df[company_df['previousClose'] > 1.0]
 
+print(f'Updated number of companies after condition 2: {len(company_df)}')
+
 
 '''
 Condition 3: "Companies with a total market capitalisation less than $30 million are not eligible for inclusion in Russell 
@@ -38,8 +43,67 @@ US indexes."
 
 company_df = company_df[company_df['marketCap'] >= 30e6]
 
+print(f'Updated number of companies after condition 3: {len(company_df)}')
+
 
 '''
 Condition 4: "Companies with less than an absolute 5% of shares available will be removed from eligibility."
+Use the formula (floatShares / sharesOutstanding) * 100 to find available share percentage.
 '''
 
+company_df['floatPercentage'] = (company_df['floatShares'] / company_df['sharesOutstanding']) * 100
+
+company_df = company_df[company_df['floatPercentage'] >= 5]
+
+print(f'Updated number of companies after condition 4: {len(company_df)}')
+
+
+'''
+Condition 5: "Companies structured in the following ways are excluded from inclusion in Russell indexes: royalty trusts, US 
+limited liability companies, closed-end investment companies (as a point of clarification: companies that are required to 
+report acquired fund fees and expenses as defined by the SEC, including business development companies, are not eligible for 
+inclusion), blank-cheque companies, special-purpose acquisition companies (SPACs), and limited partnerships. Exchange traded 
+funds (ETFs) and mutual funds are also excluded."
+'''
+
+# find out how to do this
+
+
+'''
+Condition 6: UBTI Screening
+'''
+
+# also clarify this
+
+
+'''
+Condition 7: "The following share types are not eligible for inclusion: preferred and convertible preferred stock, redeemable 
+shares, participating preferred stock, warrants, rights, depositary receipts, installment receipts and trust receipts."
+'''
+
+
+'''
+Condition 8: "Stocks must be listed on the rank day and FTSE Russell must have access to documentation on that date supporting 
+the company's eligibility for index inclusion. This includes corporate description, verification of incorporation, number of 
+shares outstanding and other information needed to determine eligibility. IPOs will be considered for index inclusion on a 
+quarterly basis."
+'''
+
+
+'''
+Condition 9: "Companies assigned a developed market nationality are required to have 5% or greater of the company's voting 
+rights (aggregated across all of its equity securities, including, where identifiable, those that are not listed or trading) 
+in the hands of unrestricted shareholders or they will be deemed ineligible for index inclusion. Shares referenced as non-voting 
+or provide legally minimum rights only will be viewed as having no voting power as it relates to the minimum voting rights 
+review. Emerging market securities are not subject to this requirement."
+'''
+
+emerging_markets = set(["Brazil", "Chile", "Colombia", "Mexico", "Peru",
+                    "Czech Republic", "Egypt", "Greece", "Hungary", "Kuwait", "Poland", "Qatar", "Saudi Arabia", "South Africa",
+                    "Turkey", "United Arab Emirates",
+                    "China", "India", "Indonesia", "Korea", "Malaysia", "Philippines", "Taiwan", "Thailand"])
+
+#company_df = company_df[company_df['country'].isin(emerging_markets)] # need to find voting rights info
+
+
+company_df.to_csv("filtered_companies.csv")
--- find_top.py
+++ find_top.py
@@ -2,6 +2,12 @@ from yahoo_fin import stock_info as si
 import yfinance as yf
 import pandas as pd
 import time
+import concurrent.futures
+import queue
+
+
+parallel = True
+
 
 start_time = time.time()
 
@@ -16,7 +22,9 @@ other_tickers = si.tickers_other()
 
 
 all_tickers = list(set(sp500_tickers + nasdaq_tickers + dow_tickers + other_tickers))
+all_tickers = [s for s in all_tickers if s.strip()]
 num_tickers = len(all_tickers)
+counter = 0
 
 print(f"Total number of unique tickers: {num_tickers}")
 
@@ -25,24 +33,113 @@ print(f"Total number of unique tickers: {num_tickers}")
 Check yfinance for ticker data and add it to the table.
 '''
 
-data = []
+market_cap_unavailable = 0
 
-for i, ticker in enumerate(all_tickers):
-    print(f"{i} / {num_tickers}")
+errored = 0
+
+def delayed_fetch(ticker):
+    time.sleep(0.5)
+    return fetch_stock_info(ticker)
+
+def fetch_stock_info(ticker):
+    global counter, market_cap_unavailable
     try:
         stock = yf.Ticker(ticker)
         stock_info = stock.info
         market_cap = stock_info.get('marketCap', 'N/A')
         if market_cap != 'N/A':
-            data.append(stock_info)
-            
+            counter += 1
+            print(f"Found data for {ticker}.")
+            return (stock_info, 1)
         else:
-            print(f"Market cap not available for {ticker}.")
+            #print(f"Market cap not available for {ticker}.")
+            market_cap_unavailable += 1
+            counter += 1
+            return (ticker, 2)
     except Exception as e:
-        print(f"Error fetching data for {ticker}: {e}")
+        #print(f"Error fetching data for {ticker}")
+        return (ticker, 0)
+    
+
+data = []
+error_tickers = queue.Queue()
+
+error_counter = {}
+
+def worker():
+    global error_counter, counter
+    while True:
+        try:
+            ticker = error_tickers.get(timeout=5)
+        except queue.Empty:
+            return
+        if ticker is None:
+            break
+        result = fetch_stock_info(ticker)
+        if result[1] == 1:
+            data.append(result[0])
+        elif result[1] == 0:
+            if error_counter.get(result[0], 0) <= 10:
+                print(f"Number of errors for {result[0]}: {error_counter.get(result[0], 0)} / {10}")
+                error_tickers.put(result[0])
+            error_counter[result[0]] = error_counter.get(result[0], 0) + 1
+        print(f"Queue size: {error_tickers.qsize()}")
+        error_tickers.task_done()
+        time.sleep(0.5)
+
+
+
+if parallel:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_ticker = {executor.submit(delayed_fetch, ticker): ticker for ticker in all_tickers}
+        for i, future in enumerate(concurrent.futures.as_completed(future_to_ticker)):
+            if i % 10 == 0:
+                print(f"Checking {i} / {num_tickers}")
+            result = future.result()
+            if result[1] == 1:
+                data.append(result[0])
+            elif result[1] == 0:
+                error_tickers.put(result[0])
+                
+    errored = error_tickers.qsize()
+    counter = 0
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(worker) for _ in range(5)]
+
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
+
+else:
+    for i, ticker in enumerate(all_tickers):
+        if i % 10 == 0:
+            print(f"{i} / {num_tickers}")
+        try:
+            stock = yf.Ticker(ticker)
+            stock_info = stock.info
+            market_cap = stock_info.get('marketCap', 'N/A')
+            if market_cap != 'N/A':
+                data.append(stock_info)
+        except Exception as e:
+            print(f"Error fetching data for {ticker}: {e}")
+            error_tickers.append(ticker)
+    while len(error_tickers) > 0:
+        print(f"Remaining tickers to check: {len(error_tickers)}")
+        ticker = error_tickers.pop()
+        try:
+            stock = yf.Ticker(ticker)
+            stock_info = stock.info
+            market_cap = stock_info.get('marketCap', 'N/A')
+            if market_cap != 'N/A':
+                data.append(stock_info)
+        except Exception as e:
+            print(f"Error fetching data for {ticker}: {e}")
+            error_tickers.append(ticker)
+
 
 
 df = pd.DataFrame(data)
+print("Table created.")
 df = df.sort_values(by='marketCap', ascending=False).reset_index(drop=True)
 
 
@@ -53,4 +150,5 @@ df.to_csv("top_companies.csv")
 
 end_time = time.time()
 
-print(f'Data collection took {end_time - start_time} to complete.')
+print(f'Data collection took {end_time - start_time} seconds to complete.')
+print(f"Total of {market_cap_unavailable} / {num_tickers} non-available market cap tickers.")
