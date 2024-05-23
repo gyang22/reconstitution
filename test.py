5e8cac16ff4d0679f2c16c53c901e260a3e4fe07
Grant Yang <gyang276@berkeley.edu>
2024-05-22 21:51:18 -0400

    SPY constituents

--- test.py
+++ test.py
@@ -1,28 +1,8 @@
-import requests
-import yfinance as yf
 import pandas as pd
 
 
-api_key = "4MHMP7DK6V0HBRIO"
-ticker = "AMZN"
-
-url = f'https://www.alphavantage.co/query?function=OVERVIEW&symbol={ticker}&apikey={api_key}'
-# r = requests.get(url)
-# data = r.json()
 
 
+spy_historical_constituents = pd.read_csv("SPY_constituents.csv")
 
-def get_market_cap(ticker):
-    try:
-        stock = yf.Ticker(ticker)
-        market_cap = stock.info['marketCap']
-        return market_cap
-    except KeyError:
-        return None
-
-sp500_constituents = pd.read_csv("constituents.csv")
-
-tickers = sp500_constituents["Symbol"].to_list()
-
-for ticker in tickers:
-    pass
+print(spy_historical_constituents)
